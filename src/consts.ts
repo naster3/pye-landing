550e85fd7029d85399f672b4825bf2ab80c37e5e
Daril Rodriguez <me@daril.dev>
2025-12-01 11:48:44 -0300

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,13 @@
+export const PYETIMES_URL = "https://pyetimes.com";
+export const GITHUB_URL = "https://github.com/somospye";
+export const DISCORD_INVITE_URL = "https://discord.gg/programacion";
+export const TWITTER_URL = "https://x.com/somospye";
+export const INSTAGRAM_URL = "https://www.instagram.com/somospye";
+export const YOUTUBE_URL = "https://www.youtube.com/@somospye";
+
+export const NAVIGATION_LINKS = [
+    { name: "Unirme", href: DISCORD_INVITE_URL },
+    { name: "Contactos", href: "#contact" },
+    { name: "GitHub", href: GITHUB_URL },
+    { name: "PyE Times", href: PYETIMES_URL },
+];
